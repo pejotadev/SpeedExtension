b34a112a8bdbb40712f94b0149a90be2e4d41f4e
Pedro José <pedrojosepaiva@outlook.com>
2020-08-06 20:29:38 -0300

    btnSpeed do not working

--- content.js
+++ content.js
@@ -22,5 +22,41 @@ const interval = setInterval(() => {
         })
 
         header.appendChild(button)
+        
     }
+    const btnSpeed = () => {
+        document.querySelectorAll("audio").forEach(audio =>{
+            audio.onplaying = () => {
+                const local = audio.parentElement.parentElement.parentElement
+                const element = document.createElement("span")
+                const textElement = document.createTextNode(">>")
+                element.appendChild(textElement) 
+                element.classList.add('btnSpeed')
+    
+                
+                element.addEventListener("click", () =>{
+                    const audios = document.querySelectorAll("audio")
+                    audios.forEach(audio => {
+                        if (audio.playbackRate == 2) {
+                            audio.playbackRate = 1
+                            element.style.color = "grey"  
+                        }else{
+                            audio.playbackRate = 2
+                            element.style.color = "#039be5"
+                        }  
+                    })
+                })
+                
+                if(local.lastChild.tagName != "SPAN")
+                    local.appendChild(element)
+                
+            }
+        })
+    }
+    btnSpeed()
+
 }, 1000)
+
+
+
+
