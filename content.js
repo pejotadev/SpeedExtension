6ac82a2764c463914ac59cf6e1fd25a2b2d9b490
Pedro José <pedrojosepaiva@outlook.com>
2020-08-06 00:56:52 -0300

    Colocando metodo que muda playbackRate

--- content.js
+++ content.js
@@ -1,1 +1,19 @@
-console.log("Minha extençao")
+
+const interval = setInterval(() => {
+    const header = document.querySelector("._1QUKR")
+    if (header) {
+        console.log(header)
+        clearInterval(interval)
+
+        const button = document.createElement("button")
+        button.innerHTML = "2x"
+        button.classList.add("twoTimeButton")
+
+        button.addEventListener("click", () =>{
+            const audios = document.querySelectorAll("audios")
+            audios.forEach(audio => { audio.playbackRate = 2 })
+        })
+
+        header.appendChild(button)
+    }
+}, 1000)
